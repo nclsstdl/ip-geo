5a7f2ea3b597aad69dcfe9b9ab81507dd03249e6
Niclas Steidle <niclassteidle@pflegix.de>
2024-06-20 21:01:10 +0200

    add colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-800": "#142d23",
+        "green-500": "#64e1af",
+        "green-100": "#c1f3df",
+        "green-50": "#effcf7",
+      },
+    },
   },
   plugins: [],
 };
