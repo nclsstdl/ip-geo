adc7943398bf0ee46757f844f3628b3125c14ede
Niclas Steidle <niclassteidle@pflegix.de>
2024-06-23 08:15:12 +0200

    add interim validation for ip address

--- src/api/ip/ip.router.ts
+++ src/api/ip/ip.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import IpController from "./ip.controller";
+import { validateIpAddress } from "./validators/ip-address.validator";
 
 export const ipRouter: Router = (() => {
   const router = Router();
 
-  router.get("/:ip/geo", IpController.getGeoInformationByIp);
+  router.get("/:ip/geo", validateIpAddress, IpController.getGeoInformationByIp);
 
   return router;
 })();
